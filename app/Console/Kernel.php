58477c79ca438993ca22229b4622c498333d2bd4
a.zubkov <zubkoffalex4nder@yandex.ru>
2025-01-27 20:37:47 +0300

    Task Scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\Product;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +17,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+            $response = Http::post('http://webhook.site', [
+                'product' => Product::whereRaw('id = (select max(`id`) from orders)')->get(),
+            ]);
+
+        })->everyFiveMinutes();
     }
 
     /**
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -14,6 +13,11 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
+
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
